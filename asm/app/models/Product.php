518af5febfe5980abe7669b8b51fcc860736e30b
thienth32 <64114903+thienth32@users.noreply.github.com>
2022-01-21 10:28:37 +0700

    khung asm1

--- asm/app/models/Product.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace App\Models;
-class Product extends BaseModel{
-    protected $tableName = 'products';
-}
-?>
--- asm/index.php
+++ asm/index.php
@@ -4,39 +4,46 @@ $url = isset($_GET['url']) ? $_GET['url'] : "/";
 use App\Controllers\HomeController;
 use App\Controllers\ProductController;
 switch ($url) {
-    case '/':
+    case 'login':
         // trang chủ - hiển thị danh sách sản phẩm theo danh mục
         $ctr = new HomeController();
         $ctr->index();
         break;
-    case 'san-pham':
+    case 'dashboard':
         // trang chi tiết sản phẩm sẽ có tham số đường dẫn ?id=xxx
         // hiển thị thông tin chi tiết của sản phẩm
         $ctr = new ProductController();
         $ctr->index();
         break;
-    case 'san-pham/tao-moi':
+    case 'mon-hoc':
         // hiển thị giao diện form tạo mới sản phẩm
         break;
-    case 'san-pham/luu-tao-moi':
+    case 'mon-hoc/tao-moi':
         // Thực hiện nhận dữ liệu từ màn hình tạo mới
         // lưu dữ liệu vào db 
         // sau đó điều hướng về trang chủ
         break;
-    case 'san-pham/cap-nhat':
-        // có tham số ?id=xxx
-        // hiển thị giao diện form cập nhật thông tin sản phẩm
+    case 'mon-hoc/luu-tao-moi':
         break;
-    case 'san-pham/luu-cap-nhat':
-        // Thực hiện nhận dữ liệu từ màn hình cập nhật
-        // lưu dữ liệu vào db 
-        // sau đó điều hướng về trang chủ
+    case 'mon-hoc/xoa':
         break;
-    case 'san-pham/xoa':
-        // có tham số đường dẫn ?id=xxx
-        // thực hiện xóa sản phẩm khỏi db
-        // sau đó điều hướng về trang chủ
+    case 'mon-hoc/chi-tiet':
         break;
+    case 'mon-hoc/cap-nhat':
+        break;
+    case 'mon-hoc/luu-cap-nhat':
+        break;
+    case 'mon-hoc/quizs':
+        break;
+    case 'mon-hoc/chi-tiet-quizs':
+        break;
+    case 'mon-hoc/quizs/tao-moi':
+        break;
+    case 'mon-hoc/quizs/luu-tao-moi':
+        break;
+    case 'mon-hoc/quizs/llam-quiz':
+        break;
+    
     default:
         # code...
         break;
